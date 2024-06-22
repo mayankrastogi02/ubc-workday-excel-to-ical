b8d12ba0838101ce87d2cf82cbd983ad70d657f8
Mayank Rastogi <mayankrustey2002@gmail.com>
2024-06-22 11:21:15 -0700

    Fixed the cookbooks

--- app/components/guides/Guide.tsx
+++ app/components/guides/Guide.tsx
@@ -11,7 +11,7 @@ function Guide() {
                         Get course schedule from UBC Workday
                     </div>
                     <div className="collapse-content">
-                        <iframe src="https://scribehow.com/embed/Accessing_and_Downloading_Course_Information_in_Workday__zWMAv7lvRpC12h39VmG4Gw?as=scrollable" width="100%" height="640" allowfullscreen frameborder="0"></iframe>
+                        <iframe src="https://scribehow.com/embed/Accessing_and_Downloading_Course_Information_in_Workday__zWMAv7lvRpC12h39VmG4Gw?as=scrollable" width="100%" height="640"></iframe>
                     </div>
                 </div>
                 <div tabIndex={0} className="collapse collapse-arrow border border-base-300 bg-base-200">
@@ -19,7 +19,7 @@ function Guide() {
                         Add calendar file to Google Calendar
                     </div>
                     <div className="collapse-content">
-                        <iframe src="https://scribehow.com/embed/Importing_UBC_Winter_2025_Calendar_into_Google_Calendar__HS7J0rjeT-SA44Z8EwcknQ?as=scrollable" width="100%" height="640" allowfullscreen frameborder="0"></iframe>
+                        <iframe src="https://scribehow.com/embed/Importing_UBC_Winter_2025_Calendar_into_Google_Calendar__HS7J0rjeT-SA44Z8EwcknQ?as=scrollable" width="100%" height="640"></iframe>
                     </div>
                 </div>
             </div>
